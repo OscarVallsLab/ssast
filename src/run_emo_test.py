0643faf1838bee13b67106d2a3e9e0afcb28e14c
OscarVallsLab <oscarvalls.cvblab@gmail.com>
2023-02-02 17:06:13 +0100

    Inference script added

---
+++ src/run_emo_test.py
@@ -0,0 +1,108 @@
+import torch
+import pickle
+from models.ast_models import ASTModel
+import torch.nn as nn
+from utilities import *
+import time
+import os
+import dataloader
+
+# Load arguments and audio config for the inference test
+with open('./finetune/IEMOCAP/exp/frame/args.pkl','rb') as file:
+    args = pickle.load(file)
+print(args.pretrained_mdl_path)
+
+val_audio_conf = {'num_mel_bins': args.num_mel_bins, 'target_length': args.target_length, 'freqm': 0, 'timem': 0, 'mixup': 0, 'dataset': args.dataset,
+                  'mode': 'evaluation', 'mean': args.dataset_mean, 'std': args.dataset_std, 'noise': False}
+
+# Create model object and load weights from state_dict
+audio_model = ASTModel(label_dim=args.n_class, fshape=args.fshape, tshape=args.tshape, fstride=args.fstride, tstride=args.tstride,
+                       input_fdim=args.num_mel_bins, input_tdim=args.target_length, model_size=args.model_size, pretrain_stage=False,
+                       load_pretrained_mdl_path='../pretrained_model/SSAST-Base-Frame-400.pth')
+
+val_loader = torch.utils.data.DataLoader(
+    dataloader.AudioDataset(dataset_json_file='./finetune/IEMOCAP/data/datafiles/1_fold_valid_data.json',
+                            label_csv='./finetune/IEMOCAP/data/IEMOCAP_class_labels_indices.csv',
+                            audio_conf=val_audio_conf),
+    batch_size=args.batch_size * 2, shuffle=False, num_workers=8, pin_memory=False)
+
+def validate(audio_model, val_loader, args, epoch):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    batch_time = AverageMeter()
+    if not isinstance(audio_model, nn.DataParallel):
+        audio_model = nn.DataParallel(audio_model)
+    audio_model = audio_model.to(device)
+    # switch to evaluate mode
+    audio_model.eval()
+
+    end = time.time()
+    A_predictions = []
+    A_targets = []
+    A_loss = []
+    with torch.no_grad():
+        for i, (audio_input, labels) in enumerate(val_loader):
+            audio_input = audio_input.to(device)
+
+            # compute output
+            audio_output = audio_model(audio_input, args.task)
+            audio_output = torch.sigmoid(audio_output)
+            predictions = audio_output.to('cpu').detach()
+
+            A_predictions.append(predictions)
+            A_targets.append(labels)
+
+            # compute the loss
+            labels = labels.to(device)
+            if isinstance(args.loss_fn, torch.nn.CrossEntropyLoss):
+                loss = args.loss_fn(audio_output, torch.argmax(labels.long(), axis=1))
+            else:
+                loss = args.loss_fn(audio_output, labels)
+            A_loss.append(loss.to('cpu').detach())
+
+            batch_time.update(time.time() - end)
+            end = time.time()
+
+        audio_output = torch.cat(A_predictions)
+        target = torch.cat(A_targets)
+        loss = np.mean(A_loss)
+        stats = calculate_stats(audio_output, target)
+
+        # save the prediction here
+        exp_dir = './finetune/IEMOCAP/exp/frame'
+        if os.path.exists(exp_dir+'/predictions') == False:
+            os.mkdir(exp_dir+'/predictions')
+            np.savetxt(exp_dir+'/predictions/target.csv', target, delimiter=',')
+        np.savetxt(exp_dir+'/predictions/predictions_' + str(epoch) + '.csv', audio_output, delimiter=',')
+
+    return stats, loss
+
+if args.data_eval != None:
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    sd = torch.load('./finetune/IEMOCAP/exp/frame/best_audio_model.pth', map_location=device)
+    if not isinstance(audio_model, torch.nn.DataParallel):
+        audio_model = torch.nn.DataParallel(audio_model)
+    audio_model.load_state_dict(sd, strict=False)
+
+    # best models on the validation set
+    args.loss_fn = torch.nn.BCEWithLogitsLoss()
+    stats, _ = validate(audio_model, val_loader, args, 'valid_set')
+    # note it is NOT mean of class-wise accuracy
+    val_acc = stats[0]['acc']
+    val_mAUC = np.mean([stat['auc'] for stat in stats])
+    print('---------------evaluate on the validation set---------------')
+    print("Accuracy: {:.6f}".format(val_acc))
+    print("AUC: {:.6f}".format(val_mAUC))
+
+    # test the models on the evaluation set
+    eval_loader = torch.utils.data.DataLoader(
+        dataloader.AudioDataset(dataset_json_file='./finetune/IEMOCAP/data/datafiles/test_data.json',
+                            label_csv='./finetune/IEMOCAP/data/IEMOCAP_class_labels_indices.csv',
+                            audio_conf=val_audio_conf),
+    batch_size=1, shuffle=True, num_workers=4, pin_memory=True)
+    stats, _ = validate(audio_model, eval_loader, args, 'eval_set')
+    eval_acc = stats[0]['acc']
+    eval_mAUC = np.mean([stat['auc'] for stat in stats])
+    print('---------------evaluate on the test set---------------')
+    print("Accuracy: {:.6f}".format(eval_acc))
+    print("AUC: {:.6f}".format(eval_mAUC))
+    np.savetxt(exp_dir + '/test_result.csv', [val_acc, val_mAUC, eval_acc, eval_mAUC])
