081397b29cacdd6e83a2397fb42c45ded9faa71e
OscarVallsLab <oscarvalls.cvblab@gmail.com>
2023-02-22 09:37:13 +0100

    Training scripts

--- src/finetune/IEMOCAP/train_avg_1_fold.sh
+++ src/finetune/IEMOCAP/train_avg_1_fold.sh
@@ -45,11 +45,146 @@ model_size=base
 head_lr=1
 pretrain_path=./${pretrain_exp}/${pretrain_model}.pth
 
-##### EXPERIMENT 6 ####
+##### EXPERIMENT 15 ####
 # Hyper parameters
-frozen_blocks=7
-ft_exp=13
-lr=1e-3
+frozen_blocks=6
+ft_exp=15
+lr=1e-4
+lr_decay=0.75
+drop_rate=0.4
+noise=True
+mixup=0
+
+# Experiment directory
+exp_name=test_${ft_exp}-f$fstride-t$tstride-b$batch_size-${model_size}-${task}-lr${lr}-lr_decay${lr_decay}-noise${noise}-drop${drop_rate}-fr_blocks${frozen_blocks}_weighted
+exp_dir=./exp/avg_tok/${exp_name}/1_fold
+
+CUDA_CACHE_DISABLE=1 python3 -W ignore ../../run.py --dataset ${dataset} \
+--data-train ${tr_data} --data-val ${val_data} --data-eval ${eval_data} --exp-dir $exp_dir \
+--label-csv ./data/IEMOCAP_class_labels_indices.csv --n_class 6 \
+--lr $lr --n-epochs ${epoch} --batch-size $batch_size --save_model False \
+--freqm $freqm --timem $timem --mixup ${mixup} --bal ${bal} \
+--tstride $tstride --fstride $fstride --fshape ${fshape} --tshape ${tshape} --warmup True --task ${task} \
+--model_size ${model_size} --adaptschedule False \
+--pretrained_mdl_path ${pretrain_path} \
+--dataset_mean ${dataset_mean} --dataset_std ${dataset_std} --target_length ${target_length} \
+--num_mel_bins 128 --head_lr ${head_lr} --noise ${noise} \
+--lrscheduler_start 5 --lrscheduler_step 1 --lrscheduler_decay ${lr_decay} --wa False --loss CE --metrics acc \
+--drop_rate ${drop_rate} --frozen_blocks ${frozen_blocks}
+
+##### EXPERIMENT 16 ####
+# Hyper parameters
+frozen_blocks=5
+ft_exp=16
+lr=1e-4
+lr_decay=0.75
+drop_rate=0.4
+noise=True
+mixup=0
+
+# Experiment directory
+exp_name=test_${ft_exp}-f$fstride-t$tstride-b$batch_size-${model_size}-${task}-lr${lr}-lr_decay${lr_decay}-noise${noise}-drop${drop_rate}-fr_blocks${frozen_blocks}_weighted
+exp_dir=./exp/avg_tok/${exp_name}/1_fold
+
+CUDA_CACHE_DISABLE=1 python3 -W ignore ../../run.py --dataset ${dataset} \
+--data-train ${tr_data} --data-val ${val_data} --data-eval ${eval_data} --exp-dir $exp_dir \
+--label-csv ./data/IEMOCAP_class_labels_indices.csv --n_class 6 \
+--lr $lr --n-epochs ${epoch} --batch-size $batch_size --save_model False \
+--freqm $freqm --timem $timem --mixup ${mixup} --bal ${bal} \
+--tstride $tstride --fstride $fstride --fshape ${fshape} --tshape ${tshape} --warmup True --task ${task} \
+--model_size ${model_size} --adaptschedule False \
+--pretrained_mdl_path ${pretrain_path} \
+--dataset_mean ${dataset_mean} --dataset_std ${dataset_std} --target_length ${target_length} \
+--num_mel_bins 128 --head_lr ${head_lr} --noise ${noise} \
+--lrscheduler_start 5 --lrscheduler_step 1 --lrscheduler_decay ${lr_decay} --wa False --loss CE --metrics acc \
+--drop_rate ${drop_rate} --frozen_blocks ${frozen_blocks}
+
+##### EXPERIMENT 17 ####
+# Hyper parameters
+frozen_blocks=4
+ft_exp=17
+lr=1e-4
+lr_decay=0.75
+drop_rate=0.4
+noise=True
+mixup=0
+
+# Experiment directory
+exp_name=test_${ft_exp}-f$fstride-t$tstride-b$batch_size-${model_size}-${task}-lr${lr}-lr_decay${lr_decay}-noise${noise}-drop${drop_rate}-fr_blocks${frozen_blocks}_weighted
+exp_dir=./exp/avg_tok/${exp_name}/1_fold
+
+CUDA_CACHE_DISABLE=1 python3 -W ignore ../../run.py --dataset ${dataset} \
+--data-train ${tr_data} --data-val ${val_data} --data-eval ${eval_data} --exp-dir $exp_dir \
+--label-csv ./data/IEMOCAP_class_labels_indices.csv --n_class 6 \
+--lr $lr --n-epochs ${epoch} --batch-size $batch_size --save_model False \
+--freqm $freqm --timem $timem --mixup ${mixup} --bal ${bal} \
+--tstride $tstride --fstride $fstride --fshape ${fshape} --tshape ${tshape} --warmup True --task ${task} \
+--model_size ${model_size} --adaptschedule False \
+--pretrained_mdl_path ${pretrain_path} \
+--dataset_mean ${dataset_mean} --dataset_std ${dataset_std} --target_length ${target_length} \
+--num_mel_bins 128 --head_lr ${head_lr} --noise ${noise} \
+--lrscheduler_start 5 --lrscheduler_step 1 --lrscheduler_decay ${lr_decay} --wa False --loss CE --metrics acc \
+--drop_rate ${drop_rate} --frozen_blocks ${frozen_blocks}
+
+##### EXPERIMENT 18 ####
+# Hyper parameters
+frozen_blocks=3
+ft_exp=18
+lr=1e-4
+lr_decay=0.75
+drop_rate=0.4
+noise=True
+mixup=0
+
+# Experiment directory
+exp_name=test_${ft_exp}-f$fstride-t$tstride-b$batch_size-${model_size}-${task}-lr${lr}-lr_decay${lr_decay}-noise${noise}-drop${drop_rate}-fr_blocks${frozen_blocks}_weighted
+exp_dir=./exp/avg_tok/${exp_name}/1_fold
+
+CUDA_CACHE_DISABLE=1 python3 -W ignore ../../run.py --dataset ${dataset} \
+--data-train ${tr_data} --data-val ${val_data} --data-eval ${eval_data} --exp-dir $exp_dir \
+--label-csv ./data/IEMOCAP_class_labels_indices.csv --n_class 6 \
+--lr $lr --n-epochs ${epoch} --batch-size $batch_size --save_model False \
+--freqm $freqm --timem $timem --mixup ${mixup} --bal ${bal} \
+--tstride $tstride --fstride $fstride --fshape ${fshape} --tshape ${tshape} --warmup True --task ${task} \
+--model_size ${model_size} --adaptschedule False \
+--pretrained_mdl_path ${pretrain_path} \
+--dataset_mean ${dataset_mean} --dataset_std ${dataset_std} --target_length ${target_length} \
+--num_mel_bins 128 --head_lr ${head_lr} --noise ${noise} \
+--lrscheduler_start 5 --lrscheduler_step 1 --lrscheduler_decay ${lr_decay} --wa False --loss CE --metrics acc \
+--drop_rate ${drop_rate} --frozen_blocks ${frozen_blocks}
+
+##### EXPERIMENT 19 ####
+# Hyper parameters
+frozen_blocks=2
+ft_exp=19
+lr=1e-4
+lr_decay=0.75
+drop_rate=0.4
+noise=True
+mixup=0
+
+# Experiment directory
+exp_name=test_${ft_exp}-f$fstride-t$tstride-b$batch_size-${model_size}-${task}-lr${lr}-lr_decay${lr_decay}-noise${noise}-drop${drop_rate}-fr_blocks${frozen_blocks}_weighted
+exp_dir=./exp/avg_tok/${exp_name}/1_fold
+
+CUDA_CACHE_DISABLE=1 python3 -W ignore ../../run.py --dataset ${dataset} \
+--data-train ${tr_data} --data-val ${val_data} --data-eval ${eval_data} --exp-dir $exp_dir \
+--label-csv ./data/IEMOCAP_class_labels_indices.csv --n_class 6 \
+--lr $lr --n-epochs ${epoch} --batch-size $batch_size --save_model False \
+--freqm $freqm --timem $timem --mixup ${mixup} --bal ${bal} \
+--tstride $tstride --fstride $fstride --fshape ${fshape} --tshape ${tshape} --warmup True --task ${task} \
+--model_size ${model_size} --adaptschedule False \
+--pretrained_mdl_path ${pretrain_path} \
+--dataset_mean ${dataset_mean} --dataset_std ${dataset_std} --target_length ${target_length} \
+--num_mel_bins 128 --head_lr ${head_lr} --noise ${noise} \
+--lrscheduler_start 5 --lrscheduler_step 1 --lrscheduler_decay ${lr_decay} --wa False --loss CE --metrics acc \
+--drop_rate ${drop_rate} --frozen_blocks ${frozen_blocks}
+
+##### EXPERIMENT 20 ####
+# Hyper parameters
+frozen_blocks=1
+ft_exp=20
+lr=1e-4
 lr_decay=0.75
 drop_rate=0.4
 noise=True
