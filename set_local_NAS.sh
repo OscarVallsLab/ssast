eb194e97d71b3426afbfdc248796e3f3bd9c123e
OscarVallsLab <oscarvalls.cvblab@gmail.com>
2023-06-06 13:49:07 +0200

    Changed NAS structure

--- set_local_NAS.sh
+++
@@ -1,6 +0,0 @@
-cd ../
-if [ -d NASFolder ] 
-then
-mkdir NASFolder
-fi
-sudo sshfs osvallo@cvblab.synology.me:/CVBLab/Proyectos/DIPSY/INVESTIGACION/Speech/data_folder /home/oscar/dipsy/ssast/NASFolder -o allow_other -p 1098
